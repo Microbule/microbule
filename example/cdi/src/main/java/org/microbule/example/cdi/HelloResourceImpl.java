f36df037274f2b35c3c8963ab475fa2bf52fadff
James Carman <james.carman@contractor.cengage.com>
2016-09-21 07:51:13 -0400

    Using @Context injection.

--- example/cdi/src/main/java/org/microbule/example/cdi/HelloResourceImpl.java
+++ example/cdi/src/main/java/org/microbule/example/cdi/HelloResourceImpl.java
@@ -1,6 +1,8 @@
 package org.microbule.example.cdi;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
 import org.ops4j.pax.cdi.api.Properties;
@@ -15,9 +17,12 @@ public class HelloResourceImpl implements HelloResource {
     @QLogger
     private Logger logger;
 
+    @Context
+    private UriInfo uriInfo;
+
     @Override
     public String sayHello(String name) {
-        logger.info("Saying hello to {}...", name);
+        logger.info("Saying hello to '{}' on URI {}...", name, uriInfo.getAbsolutePath().getPath());
         return "Hello, " + name + "!";
     }
 }
