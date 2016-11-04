22b067856fed5f234c05ab862bc583dd5bfaf678
James Carman <james.carman@contractor.cengage.com>
2016-11-04 13:33:21 -0400

    Fixing sonar issues.

--- decorator/tracer/src/main/java/org/microbule/decorator/tracer/TracerConstants.java
+++ decorator/tracer/src/main/java/org/microbule/decorator/tracer/TracerConstants.java
@@ -1,8 +1,19 @@
 package org.microbule.decorator.tracer;
 
-public class TracerConstants {
+public final class TracerConstants {
+//----------------------------------------------------------------------------------------------------------------------
+// Fields
+//----------------------------------------------------------------------------------------------------------------------
 
     public static final String DEFAULT_TRACE_ID_HEADER = "Microbule-Trace-ID";
     public static final String TRACE_ID_KEY = "microbule_trace_id";
     public static final String REQUEST_ID_KEY = "microbule_request_id";
+
+//----------------------------------------------------------------------------------------------------------------------
+// Constructors
+//----------------------------------------------------------------------------------------------------------------------
+
+    private TracerConstants() {
+
+    }
 }
