b6cec33dc85583358fd0f0b656681b505100b7db
James Carman <james.carman@contractor.cengage.com>
2016-11-06 16:03:42 -0500

    Improving code coverage.

--- decorator/tracer/src/test/java/org/microbule/decorator/tracer/TracerServerDecoratorTest.java
+++ decorator/tracer/src/test/java/org/microbule/decorator/tracer/TracerServerDecoratorTest.java
@@ -1,6 +1,11 @@
 package org.microbule.decorator.tracer;
 
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.junit.Rule;
+import org.junit.Test;
 import org.microbule.core.JaxrsServerFactoryImpl;
 import org.microbule.test.hello.HelloTestCase;
 import org.microbule.test.osgi.OsgiRule;
@@ -20,5 +25,15 @@ public class TracerServerDecoratorTest extends HelloTestCase {
     @Override
     protected void addDecorators(JaxrsServerFactoryImpl factory) {
         factory.addDecorator("tracer", new TracerServerDecorator(osgiRule.getBundleContext(), TracerConstants.DEFAULT_TRACE_ID_HEADER));
+    }
+
+    @Test
+    public void testWithCustomTraceId() {
+        final WebTarget target = createWebTarget();
+        final Response response = target.path("hello").path("Microbule")
+                .request(MediaType.TEXT_PLAIN)
+                .header(TracerConstants.DEFAULT_TRACE_ID_HEADER, "foobarbaz")
+                .get();
+        assertEquals("foobarbaz", response.getHeaderString(TracerConstants.DEFAULT_TRACE_ID_HEADER));
     }
 }
