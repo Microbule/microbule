17d64cbcd4a1533721de25383b102dbfe271e94d
James Carman <jwcarman@gmail.com>
2017-05-11 23:35:41 -0400

    Adding test cases.

--- errormap/impl/src/test/java/org/microbule/errormap/impl/WebApplicationExceptionsTest.java
+++ errormap/impl/src/test/java/org/microbule/errormap/impl/WebApplicationExceptionsTest.java
@@ -17,23 +17,12 @@
 
 package org.microbule.errormap.impl;
 
-import javax.ws.rs.core.Response;
-
 import org.junit.Test;
-import org.microbule.test.core.MockObjectTestCase;
+import org.microbule.test.core.MicrobuleTestCase;
 
-public class ResponsesTest extends MockObjectTestCase {
-//----------------------------------------------------------------------------------------------------------------------
-// Other Methods
-//----------------------------------------------------------------------------------------------------------------------
-
-    @Test
-    public void testGetErrorMessage() {
-        assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), Responses.getErrorMessage(Response.serverError().build()));
-    }
-
+public class WebApplicationExceptionsTest extends MicrobuleTestCase{
     @Test
     public void testIsUtilsClass() throws Exception {
-        assertIsUtilsClass(Responses.class);
+        assertIsUtilsClass(WebApplicationExceptions.class);
     }
 }
