91238a7bdf423414bc8a5ff2db51e306dbfef6b4
James Carman <jwcarman@gmail.com>
2017-05-13 23:30:45 -0400

    Fixing broken test case.

--- errormap/core/src/test/java/org/microbule/errormap/core/ErrorMapperServiceImplTest.java
+++ errormap/core/src/test/java/org/microbule/errormap/core/ErrorMapperServiceImplTest.java
@@ -20,49 +20,58 @@ package org.microbule.errormap.core;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 
+import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.microbule.container.core.SimpleContainer;
-import org.microbule.errormap.api.ErrorMapperUtils;
+import org.microbule.errormap.core.json.JsonErrorResponse;
+import org.microbule.errormap.core.json.JsonErrorResponseStrategy;
 import org.microbule.errormap.core.text.PlainTextErrorResponseStrategy;
+import org.microbule.gson.core.GsonServiceImpl;
+import org.microbule.gson.decorator.GsonDecorator;
 import org.microbule.test.core.MockObjectTestCase;
 
 public class ErrorMapperServiceImplTest extends MockObjectTestCase {
 
     private ErrorMapperServiceImpl service;
+    private GsonServiceImpl gsonService;
 
     @Before
     public void initService() {
         SimpleContainer container = new SimpleContainer();
         container.addBean(new PlainTextErrorResponseStrategy());
         container.addBean(new WebApplicationExceptionErrorMapper());
+        container.addBean(new JsonErrorResponseStrategy());
+        gsonService = new GsonServiceImpl(container);
+        container.addBean(new GsonDecorator(gsonService));
         service = new ErrorMapperServiceImpl(container);
         container.initialize();
     }
 
     @Test
     public void testCreateResponse() {
-        final Response response = service.createResponse(ErrorMapperUtils.DEFAULT_STRATEGY, new NotFoundException("Didn't find it!"));
+        final Response response = service.createResponse("text", new NotFoundException("Didn't find it!"));
         assertEquals(404, response.getStatus());
         assertEquals("Didn't find it!", response.readEntity(String.class));
     }
 
     @Test
     public void testCreateResponseWithNoMapper() {
-        final Response response = service.createResponse(ErrorMapperUtils.DEFAULT_STRATEGY, new RuntimeException("Didn't find it!"));
+        final Response response = service.createResponse("text", new RuntimeException("Didn't find it!"));
         assertEquals(500, response.getStatus());
         assertEquals("Didn't find it!", response.readEntity(String.class));
     }
 
     @Test
     public void testCreateException() {
-        final Exception exception = service.createException(ErrorMapperUtils.DEFAULT_STRATEGY, Response.status(Response.Status.NOT_FOUND).build());
+        final Exception exception = service.createException("text", Response.status(Response.Status.NOT_FOUND).build());
         assertEquals(NotFoundException.class, exception.getClass());
     }
 
     @Test
     public void testCreateExceptionWhenStrategyNotFound() {
-        final Exception exception = service.createException("bogus", Response.status(Response.Status.NOT_FOUND).build());
+        JsonErrorResponse errorResponse = new JsonErrorResponse(404, Lists.newArrayList("It was not found!"));
+        final Exception exception = service.createException("bogus", Response.status(Response.Status.NOT_FOUND).entity(errorResponse).build());
         assertEquals(NotFoundException.class, exception.getClass());
     }
 
