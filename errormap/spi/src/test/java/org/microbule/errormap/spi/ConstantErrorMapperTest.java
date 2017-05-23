bc5375958e35298ddbe42c9f90635fa3785e0863
James Carman <jwcarman@gmail.com>
2017-05-22 23:24:17 -0400

    Fixing unit test.

--- errormap/spi/src/test/java/org/microbule/errormap/spi/ConstantErrorMapperTest.java
+++ errormap/spi/src/test/java/org/microbule/errormap/spi/ConstantErrorMapperTest.java
@@ -26,13 +26,17 @@ import org.junit.Test;
 import org.microbule.test.core.MockObjectTestCase;
 
 public class ConstantErrorMapperTest extends MockObjectTestCase {
+//----------------------------------------------------------------------------------------------------------------------
+// Other Methods
+//----------------------------------------------------------------------------------------------------------------------
+
     @Test
     public void testMapping() {
         final ConstantErrorMapper mapper = new ConstantErrorMapper(SQLException.class, Response.Status.INTERNAL_SERVER_ERROR);
         assertEquals(SQLException.class, mapper.getExceptionType());
         final SQLException exception = new SQLException("Oops!");
+        assertEquals(Response.Status.INTERNAL_SERVER_ERROR, mapper.getStatus());
         assertEquals(Response.Status.INTERNAL_SERVER_ERROR, mapper.getStatus(exception));
         assertEquals(Lists.newArrayList("Oops!"), mapper.getErrorMessages(exception));
     }
-
 }
